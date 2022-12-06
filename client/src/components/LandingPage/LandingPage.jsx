dfd64c28bcf404b49d9197e38c295006d9577788
molinaro-77 <dmolinaro96@gmail.com>
2022-12-06 12:05:15 -0300

    estructura general de carpetas, funciones y componentes

---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function LandingPage(){
+    return (
+        <>
+        <Link
+            to='/home'>
+            <button>
+                HOME
+            </button>
+        </Link>    
+        </>
+    );
+}
+
+export default LandingPage;
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link , useLocation } from "react-router-dom";
+import logo from "../../images/logo.png";
+import './Nav.css'
+
+function Nav(){
+
+    return(
+        <>
+            {/*estructura del nav-bar*/ }
+        </>
+    )
+}
+
+export default Nav;
