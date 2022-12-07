e102e4348ec5b3b98b9858f14b01d4dc77c973ab
RedABJs <jamesanga0318@gmail.com>
2022-12-07 14:35:04 -0500

    fix(api): Fixed NFT-Collection models and added collections.json

--- api/src/models/Collection.js
+++ api/src/models/Collection.js
@@ -1,23 +1,20 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "collection",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        unique: true,
-        allowNull: false,
-        validate: {
-          isUUID: 4,
-        },
-      },
+  sequelize.define("collection", {
+    id: {
+      type: DataTypes.STRING,
+      // defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+      // validate: {
+      //   isUUID: 4,
+      // },
+    } /* 
       description: {
         type: DataTypes.STRING,
         unique: true,
-      },
-    }
-  )
-}
+      }, */,
+  });
+};
