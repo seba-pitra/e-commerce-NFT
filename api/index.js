c559ea75bda8be4299336e3bf911cc69e59a1bdf
Vaxtronn <miguel.villam96@gmail.com>
2022-12-06 19:15:07 -0500

    DB Deploy (api/): Deploy

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
-server.listen(port, () => {
-  console.log(`listening at ${3001}`);
-});
+// server.listen(port, () => {
+//   console.log(`listening at ${3001}`);
+// });
 
 // Syncing all the models at once.
-// conn.sync({ force: true }).then(() => {
-//   server.listen(3001, () => {
-//     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-//   });
-// });
+conn.sync({ alter: true }).then(() => {
+  server.listen(3001, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+  });
+});
