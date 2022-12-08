e7c3158d723575ef43f9a4519308cc314d6d3909
molinaro-77 <dmolinaro96@gmail.com>
2022-12-08 14:42:24 -0300

    price range selector version alpha

--- client/src/components/FilterOptrions/PriceSelector/PriceSelector.jsx
+++ client/src/components/FilterOptrions/PriceSelector/PriceSelector.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
 const App = () => {
 
-    const [value, setValue] =  React.useState([0,100]);
-    // Changing State when volume increases/decreases
-    const rangeSelector = (event, newValue) => {
-        setValue(newValue);
-        console.log(newValue)
+    const [range, setRange] =  useState(
+        {
+            min : 0,
+            max : 0
+        }
+    );
+
+    
+    const currencies = ['USD', 'EUR', 'GBP']
+    
+    const invalidMaxValue = range.max <= range.min;
+    
+    const handleMinChange = (e) => {
+        setRange({...range, min: parseInt(e.target.value)});
+    }
+    const handleMaxChange = (e) => {
+        setRange({...range, max: parseInt(e.target.value)});
+    }
+    
+    const handleSubmit = (e) => {
+        /*
+        dispatch range to reducer
+        */
+       alert(`el reducer recibio el rango ${range.min} - ${range.max}` )
     };
+    console.log(invalidMaxValue)
+    console.log(range)
 
     return (
-    <div style={{
-        margin: 'auto',
-        display: 'block',
-        width: 'fit-content'
-    }}>
-        <h3>Precio entre: </h3>
-        <Slider
-        value={value}
-        onChange={rangeSelector}
-        valueLabelDisplay="auto"
-        />
-        Your range of Price is between ${value[0]} and ${value[1]}
-    </div>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+            <h3>Precio : </h3>
+                <select name="" id="">
+                    {currencies.map(currency => {
+                        return <option>
+                            {currency}
+                        </option>
+                    })}
+                </select>
+            <label htmlFor="min">Min: </label>
+                <input name="min" type="number" value={range.min} onChange={(e)=>handleMinChange(e)} min="0"/>
+            <label htmlFor="max">Max: </label>
+                <input name="max" type="number" value={range.max} onChange={(e)=>handleMaxChange(e)} min={range.min}/>
+            <input type="submit" value="solicitar" disabled={invalidMaxValue}/>
+        </form>
     );
 }
 
