d4886906cb7189bd6371eb201cbe963ff1a7f944
valcoellar <valcoellar@gmail.com>
2022-12-08 19:31:31 -0600

    Landing SignUp Attempt 1

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
- 
+
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+
 function LandingPage(){
     return (
         <>
@@ -9,7 +14,89 @@ function LandingPage(){
                 HOME
             </button>
         </Link>    
-        </>
+        
+	    {/* Loguin form attempt 1*/}
+<div className="vh-50">
+  <div className="container-fluid h-custom">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://www.realestatemarket.com.mx/images/2022/05-mayo/1305/se-suman-fondos-de-inversion-proptech-latam-2022-g.jpg"
+          className="img-fluid" alt="Enter the Town" />
+      </div>
+      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+            <FacebookIcon />  
+	    </button>
+
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+           <GitHubIcon />
+	    </button>
+
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+	    <LinkedInIcon />
+            </button>
+          </div>
+
+          <div className="divider d-flex align-items-center my-4">
+            <p className="text-center fw-bold mx-3 mb-0">Or if you have an Account:</p>
+          </div>
+
+          
+          <div className="form-outline mb-4">
+            <input type="email" id="form3Example3" className="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            <label className="form-label" for="form3Example3">Email address</label>
+          </div>
+
+          
+          <div className="form-outline mb-3">
+            <input type="password" id="form3Example4" className="form-control form-control-lg"
+              placeholder="Enter password" />
+            <label className="form-label" for="form3Example4">Password</label>
+          </div>
+
+          <div className="d-flex justify-content-between align-items-center">
+     
+
+
+	    <div className="form-check mb-0">
+              <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label className="form-check-label" for="form2Example3">
+                Remember me
+              </label>
+            </div>
+            <a href="#!" className="text-body">Forgot your password?</a>
+          </div>
+
+          <div className="text-center text-lg-start mt-4 pt-2">
+            <button type="button" className="btn btn-primary btn-lg"
+              style={{paddingLeft: "2.5rem", paddingRight: "2.5rem"}}>Login</button>
+            <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                className="link-danger">Register</a></p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+	    </>
+
     );
 }
 
