0f434d34d25024ae6d71a048f92b7d34126860a7
Seba <sebastian.pitra10@gmail.com>
2022-12-09 09:51:09 -0300

    fix(/api): fix connection with database

--- api/index.js
+++ api/index.js
@@ -2,12 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
-// server.listen(port, () => {
-//   console.log(`listening at ${3001}`);
-// });
-
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, DB_DEPLOY2 } = process.env;
 
 // const sequelize = new Sequelize(
 //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerceNFTs`,
