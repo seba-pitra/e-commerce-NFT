a67f1c4d24a294d49b4cde6ac53c1b5f9ee07724
Seba <sebastian.pitra10@gmail.com>
2022-12-09 22:55:30 -0300

    add(api/routes): create collection router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const nftRouter = require("./nftRouter.js");
 const userRouter = require("./user.router");
+const collectionRouter = require("./collection.router");
 
 const router = Router();
 
 router.use("/nft", nftRouter);
 router.use("/user", userRouter);
+router.use("/collection", collectionRouter);
 
 module.exports = router;
