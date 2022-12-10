b81f3b520e96acca7f35ae9852c3659aa0a0e52d
Seba <sebastian.pitra10@gmail.com>
2022-12-09 22:53:03 -0300

    delete(models/collection): delete timestamps

--- api/src/models/Collection.js
+++ api/src/models/Collection.js
@@ -1,20 +1,26 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("collection", {
-    id: {
-      type: DataTypes.STRING,
-      // defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      unique: true,
-      allowNull: false,
-      // validate: {
-      //   isUUID: 4,
-      // },
-    } /* 
+  sequelize.define(
+    "collection",
+    {
+      id: {
+        type: DataTypes.STRING,
+        // defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        // validate: {
+        //   isUUID: 4,
+        // },
+      } /* 
       description: {
         type: DataTypes.STRING,
         unique: true,
       }, */,
-  });
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
