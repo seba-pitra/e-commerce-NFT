bc1764b06323f8aee7526d5ca2a2f1724ca86598
Seba <sebastian.pitra10@gmail.com>
2022-12-11 15:25:03 -0300

    add(components/nftDetail): create styles

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -3,19 +3,22 @@ import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../../redux/actions";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router-dom";
-import s from "./Details.css";
+import styles from "./Details.module.css";
+import ethereumLogo from "../../images/ethereum-logo.png";
 
 function Details(props) {
   //{image, name, id, owner, price, lastSale,viewsCount,SalesHistory}
   const { id } = props.match.params;
+
   const nftDetail = useSelector((state) => state.nftDetail);
   const isLoading = useSelector((state) => state.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(actions.getNftDetail(id));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [dispatch, id]);
+
+  console.log("DETALLE", nftDetail);
 
   return (
     <>
@@ -24,47 +27,69 @@ function Details(props) {
         isLoading ? (
           <Loading />
         ) : (
-          <div className={s.detailsContainer}>
-            {/*link to go back to home*/}
-            <Link className="link" to="/home">
-              <button className="details-go-back-btn">&#x3c;</button>
+          <div className={styles["detail-all-container"]}>
+            <Link to={"/home"} className={styles["back-button"]}>
+              {"< "}Back
             </Link>
-
-            {/*aca va la estructura del detalle del nft*/}
-
-            <div className={s.imageNftContainer}>
-              <div className="borderImgItems">
-                {/* podria ir cant de likes y el nombre en cada esquina como openSea */}
+            <div className={styles["detail-card-container"]}>
+              <img
+                src={nftDetail.image}
+                alt="nft-detail"
+                className={styles["nft-img"]}
+              />
+              <div className={styles["nft-data-container"]}>
+                <div>
+                  <h1>{nftDetail.name}</h1>
+                  {nftDetail.source && (
+                    <span className={styles["detail-span"]}>
+                      Included from{"  "}
+                      <img
+                        src={nftDetail.source.icon}
+                        alt="icon-detail"
+                        className={styles["source-icon"]}
+                      />
+                    </span>
+                  )}
+                </div>
+                <div className={styles["price-container"]}>
+                  <span className={styles["detail-span"]}>Price</span>
+                  <div className={styles["ethereum-container"]}>
+                    <img
+                      src={ethereumLogo}
+                      alt="ethereum-logo"
+                      className={styles["ethereum-logo-price"]}
+                    />
+                    <h3>{nftDetail.price}</h3>
+                  </div>
+                </div>
+                <div className={styles["data-detail-nft"]}>
+                  <h2>Details</h2>
+                  {nftDetail.available ? (
+                    <span className={styles.available}>Available</span>
+                  ) : (
+                    <span className={styles.unavailable}>Unavailable</span>
+                  )}
+                  <div>
+                    <span>
+                      Contract
+                      <br />
+                      adress:
+                    </span>
+                    <span className={styles["contract-adress"]}>
+                      {nftDetail.contract}
+                    </span>
+                  </div>
+                  <div>
+                    <span>Token id:</span>
+                    <span>{nftDetail.tokenId}</span>
+                  </div>
+                </div>
+                <div className={styles["buttons-container"]}>
+                  <button className={styles.button}>Add to cart</button>
+                  <button className={styles.button}>Like</button>
+                </div>
               </div>
-              <img src={nftDetail.image} alt="nft-image" />
             </div>
-            <div className={s.details}>
-              <h1>
-                {nftDetail.name} {nftDetail.tokenId}
-              </h1>
-              {/* <h2>
-                Property of : {nftDetail.source.name}/
-                {nftDetail.userId && <h2>ID : {nftDetail.userId}</h2>}
-              </h2> */}
- {/*              <img src={nftDetail.source.icon} alt="owner-icon" /> */}
-              <h4>Type: {nftDetail.type}</h4>
-              <h3>Price : {nftDetail.price}</h3>
-              {nftDetail.collectionId && (
-                <h3>Collection ID : {nftDetail.collectionId}</h3>
-              )}
-              <div className="viewsAndFavs">
-                {/* faltaria views */}
-                {/* {props.viewsCount} */}
-              </div>
-              <div className="lastSaleContainer">
-                {/* falta la info ,se podria hacer la funcion que guarde eso */}
-              </div>
-            </div>
-            <div className="description"></div>
-            <div className="price-history">
-              {/* aca deberia ir todos los precios que tuvo este nft,con fechas */}
-            </div>
-            {/* meaby a "more items like this " div */}
           </div>
         )
       }
