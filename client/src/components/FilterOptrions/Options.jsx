a560060737af0745e2d10dc381307cad1e936361
RedABJs <jamesanga0318@gmail.com>
2022-12-11 19:10:33 -0500

    Adding css to home and filter components

--- client/src/components/FilterOptrions/Options.jsx
+++ client/src/components/FilterOptrions/Options.jsx
@@ -1,27 +1,30 @@
-import * as actions from '../../redux/actions'
-import Ordering from './Ordering/Ordering';
-import Filtering from './Filtering/Filtering';
-import PriceSelector from './PriceSelector/PriceSelector';
-import NFTSPerPageSelector from './NFTSPerPageSelector/NFTSPerPageSelector';
-import { useDispatch } from 'react-redux';
+import * as actions from "../../redux/actions";
+import Ordering from "./Ordering/Ordering";
+import Filtering from "./Filtering/Filtering";
+import PriceSelector from "./PriceSelector/PriceSelector";
+import NFTSPerPageSelector from "./NFTSPerPageSelector/NFTSPerPageSelector";
+import { useDispatch } from "react-redux";
+import "./Options.css";
 
-export default function Options(){
-    const dispatch = useDispatch();
+export default function Options() {
+  const dispatch = useDispatch();
 
-    return (
-        <>
-            <Ordering/>
-            <Filtering/>
-            <PriceSelector/>
-            {/* <NFTSPerPageSelector/> */}
-            <div className='options-container'>
-            <button
-                className='option-btn'
-                onClick={()=>{dispatch(actions.resetFilters())}}
-                >Reset Filters
-            </button>
-            </div>
-        </>
-    )
-
+  return (
+    <div className="alllFilters-container">
+      <Ordering />
+      <Filtering />
+      <PriceSelector />
+      {/* <NFTSPerPageSelector/> */}
+      <div className="options-container">
+        <button
+          className="resetfilters-btn"
+          onClick={() => {
+            dispatch(actions.resetFilters());
+          }}
+        >
+          Reset Filters
+        </button>
+      </div>
+    </div>
+  );
 }
