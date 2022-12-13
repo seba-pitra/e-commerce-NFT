959cd09a8f2513e39ec30f2e79cc46a6be788e05
Seba <sebastian.pitra10@gmail.com>
2022-12-12 22:43:46 -0300

    add(components/Landing): create authentication animation

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,11 +5,16 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import img from "../../images/city-landing.jpg";
+import { useState } from "react";
 
 function LandingPage() {
+  const [customClass, setCustomClass] = useState("landing-container");
+  const [secondCustomClass, setSecondCustomClass] =
+    useState("account-container");
+
   return (
     <div className={styles["landing"]}>
-      <div className={styles["landing-container"]}>
+      <div className={styles[customClass]}>
         <div className={styles["landing-info-container"]}>
           <div className={styles["landing-text-container"]}>
             <h1 className={styles["landing-title"]}>NOT FUNGIBLE TOWN</h1>
@@ -25,9 +30,15 @@ function LandingPage() {
               </div>
             </h6>
           </div>
-          <Link to={"/home"} className={styles["link-home"]}>
-            Let's Go
-          </Link>
+          <button
+            onClick={() => {
+              setCustomClass("slide-container");
+              setSecondCustomClass("slide-account-container");
+            }}
+            className={styles["link-home"]}
+          >
+            Lets go
+          </button>
         </div>
 
         <img
@@ -37,123 +48,128 @@ function LandingPage() {
           alt="Enter the Town"
         />
       </div>
-      {/* <div className="vh-50">
-        <div className="container-fluid h-custom">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form>
-                <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                  <p className="lead fw-normal mb-0 me-3 text-light">
-                    Sign in with
-                  </p>
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-floating mx-1"
-                  >
-                    <FacebookIcon />
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-floating mx-1"
-                  >
-                    <GitHubIcon />
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-floating mx-1"
-                  >
-                    <LinkedInIcon />
-                  </button>
-                </div>
-
-                <div className="divider d-flex align-items-center my-4">
-                  <p className="text-center fw-bold mx-3 mb-0 text-light">
-                    Or if you have an Account:
-                  </p>
-                </div>
-
-                <div className="form-outline mb-4">
-                  <label className="form-label text-light" for="EmailField">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    id="EmailField"
-                    className="form-control form-control-lg"
-                    placeholder="Enter a valid email address"
-                  />
-                </div>
-
-                <div className="form-outline mb-3">
-                  <label className="form-label text-light" for="PassField">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="PassField"
-                    className="form-control form-control-lg"
-                    placeholder="Enter password"
-                  />
-                </div>
-
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="form-check mb-0">
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      value=""
-                      id="RememberCheck"
-                    />
-                    <label
-                      className="form-check-label text-light"
-                      for="RememberCheck"
+      <div className={styles[secondCustomClass]}>
+        <div className="vh-50">
+          <div className="container-fluid h-custom">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form>
+                  <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                    <p className="lead fw-normal mb-0 me-3 text-light">
+                      Sign in with
+                    </p>
+                    <button
+                      type="button"
+                      className="btn btn-dark btn-floating mx-1"
                     >
-                      Remember me
-                    </label>
+                      <FacebookIcon />
+                    </button>
+
+                    <button
+                      type="button"
+                      className="btn btn-dark btn-floating mx-1"
+                    >
+                      <GitHubIcon />
+                    </button>
+
+                    <button
+                      type="button"
+                      className="btn btn-dark btn-floating mx-1"
+                    >
+                      <LinkedInIcon />
+                    </button>
                   </div>
 
-                  <a href="#!" className=" text-light">
-                    Forgot your password?
-                  </a>
-                </div>
+                  <div className="divider d-flex align-items-center my-4">
+                    <p className="text-center fw-bold mx-3 mb-0 text-light">
+                      Or if you have an Account:
+                    </p>
+                  </div>
 
-                <div className="text-center text-lg-start mt-4 pt-2">
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-lg"
-                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                  >
-                    Login
-                  </button>
+                  <div className="form-outline mb-4">
+                    <label className="form-label text-light" for="EmailField">
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      id="EmailField"
+                      className="form-control form-control-lg"
+                      placeholder="Enter a valid email address"
+                    />
+                  </div>
 
-                  <Link to="/home">
+                  <div className="form-outline mb-3">
+                    <label className="form-label text-light" for="PassField">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="PassField"
+                      className="form-control form-control-lg"
+                      placeholder="Enter password"
+                    />
+                  </div>
+
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="form-check mb-0">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        value=""
+                        id="RememberCheck"
+                      />
+                      <label
+                        className="form-check-label text-light"
+                        for="RememberCheck"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+
+                    <a href="#!" className=" text-light">
+                      Forgot your password?
+                    </a>
+                  </div>
+
+                  <div className="text-center text-lg-start mt-4 pt-2">
                     <button
                       type="button"
                       className="btn btn-dark btn-lg"
-                      style={{
-                        paddingLeft: "2.5rem",
-                        paddingRight: "2.5rem",
-                        marginLeft: "50px",
-                      }}
+                      style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
                     >
-                      DEMO
+                      Login
                     </button>
-                  </Link>
 
-                  <p className="small fw-bold mt-2 pt-1 mb-0 text-light">
-                    Don't have an account?{" "}
-                    <a href="#!" className="link-danger">
-                      Register
-                    </a>
-                  </p>
-                </div>
-              </form>
+                    <Link to="/home">
+                      <button
+                        type="button"
+                        className="btn btn-dark btn-lg"
+                        style={{
+                          paddingLeft: "2.5rem",
+                          paddingRight: "2.5rem",
+                          marginLeft: "50px",
+                        }}
+                      >
+                        DEMO
+                      </button>
+                    </Link>
+
+                    <p className="small fw-bold mt-2 pt-1 mb-0 text-light">
+                      Don't have an account?{" "}
+                      <a href="#!" className="link-danger">
+                        Register
+                      </a>
+                    </p>
+                  </div>
+                </form>
+                <Link to={"/home"} className={styles["link-home"]}>
+                  Home
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
