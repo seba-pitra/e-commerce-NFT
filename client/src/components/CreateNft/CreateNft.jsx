adbb64cfbcd07b6b2f124c3bb927a6c9fb4cd810
brunoosuna8 <brunoosuna8@gmail.com>
2022-12-13 12:24:49 -0300

    css and minor changes

--- client/src/components/CreateNft/CreateNft.jsx
+++ client/src/components/CreateNft/CreateNft.jsx
@@ -6,23 +6,22 @@ import PreviewNft from "./PreviewNft/PreviewNft";
 import { useDispatch, useSelector } from "react-redux";
 
 export function validate(input) {
-  let errors = { 
-    name: "no data", price: "no data"
+  let errors = {
+    name: "no data",
+    price: "no data",
   };
 
   console.log(input);
 
   if (!/([A-Z])/.test(input.name)) {
-    errors = {...errors,name : 'Username is invalid'}
-    
-    
+    errors = { ...errors, name: "Username is invalid" };
   } else {
-    errors = {...errors,name : 'Name is correct'}
-  } 
-  if(input.price <= 0){
-    errors = {...errors,price : 'Price can not be 0 or less'}
-  }else{
-    errors = {...errors,price : 'Price is correct'}
+    errors = { ...errors, name: "Name is correct" };
+  }
+  if (input.price <= 0) {
+    errors = { ...errors, price: "Price can not be 0 or less" };
+  } else {
+    errors = { ...errors, price: "Price is correct" };
   }
   console.log(errors);
   return errors;
@@ -41,10 +40,8 @@ export default function Form() {
 
   const [errors, setErrors] = React.useState({
     name: "no data",
-    price:"do data"
-
+    price: "do data",
   });
-
 
   //asi tendria q venir el array de categories,puede cambiar como venga
   let categories = ["humanoid", "cyberpunk", "object", "animal"];
@@ -78,108 +75,122 @@ export default function Form() {
           <h1>Create Non Fungible Token</h1>
 
           <form className="createNft">
-            <label>Name</label>
-            <input
-              type={"text"}
-              name={"name"}
-              value={input.name}
-              onChange={(e) => handleChange(e)}
-            />
-            <h3>Image,video,audio or 3D model</h3>
-            <p>
-              File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV,
-              OGG, GLB, GLTF. Max size: 100 MB
-            </p>
+            <div className="inputContainer">
+              <label>Name</label>
+              <input
+                type={"text"}
+                name={"name"}
+                value={input.name}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            <div className="inputContainer">
+              <h3>Image,video,audio or 3D model</h3>
+              <p>
+                File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV,
+                OGG, GLB, GLTF. Max size: 100 MB
+              </p>
+            </div>
 
-            <label>External Link</label>
-            {/* aca deberia ir el input para agregar el archivo  */}
-            <input
-              type={"text"}
-              name={"link"}
-              value={input.link}
-              onChange={(e) => handleChange(e)}
-            />
+            <div className="inputContainer">
+              <label>External Link</label>
+              {/* aca deberia ir el input para agregar el archivo  */}
+              <input
+                type={"text"}
+                name={"link"}
+                value={input.link}
+                onChange={(e) => handleChange(e)}
+              />
 
-            <p>
-              It will include a link to this URL on the detail page of this item
-              so that users can click to learn more about it.
-            </p>
+              <p>
+                It will include a link to this URL on the detail page of this
+                item so that users can click to learn more about it.
+              </p>
+            </div>
 
-            <label>Description</label>
-            <input
-              type={"text"}
-              name={"description"}
-              value={input.description}
-              onChange={(e) => handleChange(e)}
-            />
-            <p>
-              The description will be included on the item's detail page below
-              its image.
-            </p>
+            <div className="inputContainer">
+              <label>Description</label>
+              <input
+                type={"text"}
+                name={"description"}
+                value={input.description}
+                onChange={(e) => handleChange(e)}
+              />
+              <p>
+                The description will be included on the item's detail page below
+                its image.
+              </p>
+            </div>
 
-            <label>Collection</label>
-            <p>This is the collection where your item will appear.</p>
-            <input
-              type={"text"}
-              name={"collection"}
-              value={input.collection}
-              onChange={(e) => handleChange(e)}
-            />
+            <div className="inputContainer">
+              <label>Collection</label>
+              <p>This is the collection where your item will appear.</p>
+              <input
+                type={"text"}
+                name={"collection"}
+                value={input.collection}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-            <label>Price</label>
-            {/* se podria agregar en dolares/ethereum ,etc */}
-            <input
-              type={"number"}
-              name={"price"}
-              value={input.price}
-              onChange={(e) => handleChange(e)}
-            />
+            <div className="inputContainer">
+              <label>Price</label>
+              {/* se podria agregar en dolares/ethereum ,etc */}
+              <input
+                type={"number"}
+                name={"price"}
+                value={input.price}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
 
-            <label>Category</label>
-            <p>
-              Add one category or more to classify it.You can select categories
-              by existing ones or add a new one
-            </p>
-            <input type={"text"} name={"category"} />
-            <button className="addCategory"></button>
+            <div className="inputContainer">
+              <label>Category</label>
+              <p>
+                Add one category or more to classify it.You can select
+                categories by existing ones or add a new one
+              </p>
+              <input type={"text"} name={"category"} />
+              <button className="addCategory"></button>
+              <select onChange={(e) => handleChangeSelect(e)} name="categories">
+                <option hidden disabled selected value>
+                  Categories
+                </option>
 
-            <select onChange={(e) => handleChangeSelect(e)} name="categories">
-              <option hidden disabled selected value>
-                Categories
-              </option>
-
-              {categories &&
-                categories.map((e) => (
-                  <option value={e} name="categories" key={e}>
-                    {e}
-                  </option>
-                ))}
-            </select>
+                {categories &&
+                  categories.map((e) => (
+                    <option value={e} name="categories" key={e}>
+                      {e}
+                    </option>
+                  ))}
+              </select>
+            </div>
           </form>
         </div>
-        <div className="ilustration-validations">
         
-        <div className="ilustrationContainer">
-          <h2>This is how your NFT it will be create</h2>
-          <PreviewNft
-            image={input.image}
-            name={input.name}
-            price={input.price}
-          />
-        </div>
-        <div className="validations">
-        <h4>{errors.name}</h4>
-        <h4>{errors.price}</h4>
-
-        </div>
-        <input
-          className='submit'
+        <div className="ilustration-validations">
+          <div className="ilustrationContainer">
+            <h2>This is how your NFT it will be create</h2>
+            <PreviewNft
+              image={input.image}
+              name={input.name}
+              price={input.price}
+            />
+          </div>
+          <div className="validations">
+            <h4>{errors.name}</h4>
+            <h4>{errors.price}</h4>
+          </div>
+          <input
+            className="submit"
             type="submit"
             value={"Create NFT"}
-            disabled={errors.name !== 'Name is correct' || errors.price !== 'Price is correct'}
+            disabled={
+              errors.name !== "Name is correct" ||
+              errors.price !== "Price is correct"
+            }
           />
         </div>
-        
       </div>
     </React.Fragment>
   );
