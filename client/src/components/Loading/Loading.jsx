a81ad7c4544cd6a851ec7aee289bb130fac14101
molinaro-77 <dmolinaro96@gmail.com>
2022-12-14 16:23:40 -0300

    loading component centered

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -4,16 +4,20 @@ import './Loading.css';
 export default function Loading(){
     return (
         <div className="loading-page-container">
-            <div class="triangles">
-                <div class="tri invert"></div>
-                <div class="tri invert"></div>
-                <div class="tri"></div>
-                <div class="tri invert"></div>
-                <div class="tri invert"></div>
-                <div class="tri"></div>
-                <div class="tri invert"></div>
-                <div class="tri"></div>
-                <div class="tri invert"></div>
+            <div className='loading-icon'>
+            <div className='triangles-container'>
+                <div class="triangles">
+                    <div class="tri invert"></div>
+                    <div class="tri invert"></div>
+                    <div class="tri"></div>
+                    <div class="tri invert"></div>
+                    <div class="tri invert"></div>
+                    <div class="tri"></div>
+                    <div class="tri invert"></div>
+                    <div class="tri"></div>
+                    <div class="tri invert"></div>
+                </div>
+            </div>
                 <h1 className="loading-message">
                 Loading..
                 </h1>
