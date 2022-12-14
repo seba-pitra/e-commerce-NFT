0ebdc21144ea9c8b1305f894d3e836c7e11fb1c9
Seba <sebastian.pitra10@gmail.com>
2022-12-14 14:20:59 -0300

    fix(components/nav): fixed

--- client/src/components/Pages/Pages.jsx
+++ client/src/components/Pages/Pages.jsx
@@ -1,41 +1,51 @@
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react'
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import NFTCard from "../NFTCard/NFTCard";
 import NotFoundResults from "../NotFoundResults/NotFoundResults";
 import PageSelector from "../PageSelector/PageSelector";
 import "./Pages.css";
 
+function Pages() {
+  const filteredNfts = useSelector((state) => state.filteredNfts);
+  const activePage = useSelector((state) => state.activePage);
+  const nftsPerPage = useSelector((state) => state.nftsPerPage);
+  const lastNftInPage = activePage * nftsPerPage;
+  const firstNftInPage = lastNftInPage - nftsPerPage;
+  const nftsInPage = filteredNfts.slice(firstNftInPage, lastNftInPage);
+  console.log(nftsInPage);
+  const cards = nftsInPage.map((nft) => {
+    return (
+      <NFTCard
+        category={nft.category}
+        collection={nft.collection}
+        collectionId={nft.collectionId}
+        contract={nft.contract}
+        id={nft.id}
+        image={nft.image}
+        name={nft.name}
+        price={nft.price}
+        source={nft.source}
+        tokenId={nft.tokenId}
+        type={nft.type}
+        userId={nft.userId}
+      />
+    );
+  });
 
-function Pages(){
-    const filteredNfts = useSelector(state => state.filteredNfts)
-    const activePage = useSelector(state => state.activePage);
-    const nftsPerPage = useSelector(state => state.nftsPerPage);
-    const lastNftInPage = activePage * nftsPerPage;
-    const firstNftInPage = lastNftInPage - nftsPerPage;
-    const nftsInPage = filteredNfts.slice(firstNftInPage, lastNftInPage);
-    const cards = nftsInPage.map((nft)=>{
-        return <NFTCard
-            image={nft.image}
-            name={nft.name}
-            id={nft.id}
-            price={nft.price}
-        />
-    });
-    
-    // console.log(nftsInPage);
-    return(
-        <div>
-            { 
-            cards.length === 0 ? 
-                <NotFoundResults/> :
-                <>
-                <PageSelector/>
-                    <div className='pageSelector-Container'>{cards}</div>
-                <PageSelector/>
-                </>
-        }
-        </div>
-    )
+  // console.log(nftsInPage);
+  return (
+    <div>
+      {cards.length === 0 ? (
+        <NotFoundResults />
+      ) : (
+        <>
+          <PageSelector />
+          <div className="pageSelector-Container">{cards}</div>
+          <PageSelector />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Pages;
