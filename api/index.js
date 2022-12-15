b3097db6af24d66777d3ac0142a8abff0e9a5f5d
Seba <sebastian.pitra10@gmail.com>
2022-12-15 10:40:30 -0300

    fix: mercado pago data

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 require("./src/firebase.js");
+require("./src/services/PaymentService");
 // server.listen(port, () => {
 //   console.log(`listening at ${3001}`);
 // });
 
-// Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
