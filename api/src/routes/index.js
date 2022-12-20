f913f425f7e46a0f1e4cf0821c209a6bfc0bf0de
molinaro-77 <dmolinaro96@gmail.com>
2022-12-20 18:42:53 -0300

    removed firebase from back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const nftRouter = require("./nftRouter.js");
 const collection = require("./collection.router");
-const firebaseRouter = require("./login.js");
 const paymentRouter = require("./paymentRouter");
 const dbRouter = require("./db.router")
 const router = Router();
@@ -9,7 +8,6 @@ const router = Router();
 router.use("/nft", nftRouter);
 router.use("/collection", collection);
 router.use("/database", dbRouter);
-router.use("/login", firebaseRouter);
 router.use("/payment", paymentRouter);
 
 module.exports = router;
