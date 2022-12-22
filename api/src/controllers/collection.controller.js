8b52434b7c866e05cfb63f3bf01fabdbd32edbf3
RedABJs <jamesanga0318@gmail.com>
2022-12-22 09:38:42 -0500

    dashboard advance

--- api/src/controllers/collection.controller.js
+++ api/src/controllers/collection.controller.js
@@ -1,51 +1,49 @@
-const { reauthenticateWithCredential } = require("firebase/auth");
 const { Collection } = require("../db");
-const { collections } = require("../jsondata/collections.json")
+const { collections } = require("../jsondata/collections.json");
 
 const getCollections = async (req, res) => {
-  try{
+  try {
     const dbCollections = await Collection.findAll();
-    if(dbCollections.length === 0){
+    if (dbCollections.length === 0) {
       throw new Error("nothing on database please contact Mr. Miguel Villa");
     }
     return res.status(200).json(dbCollections);
-  }catch(err){
-    return res.status(400).json({error : err.message});
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
   }
 };
 
 const postAllCollectionsToDB = async (req, res) => {
-  try{
+  try {
     const allCollections = createAllInitialCollections();
     res.status(200).json(allCollections);
-  }catch(err){
-    res.status(400).json({error : err.message});
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-}
+};
 
 const createAllInitialCollections = async () => {
-  try{
+  try {
     const response = await Collection.findAll();
-    if(response.length === 0){
-      collections.forEach(async collection => {
+    if (response.length === 0) {
+      collections.forEach(async (collection) => {
         let collectionToDB = {
-          id : collection.id,
-          name : collection.name || "No name",
-          image : collection.image || "No image"
+          id: collection.id,
+          name: collection.name || "No name",
+          image: collection.image || "No image",
         };
-        const collectionInDB = await Collection.create(collectionToDB)
+        const collectionInDB = await Collection.create(collectionToDB);
         response.push(collectionInDB);
-      })
+      });
     }
-      return response;
-  }catch(err) {
+    return response;
+  } catch (err) {
     throw new Error(err.message);
   }
-}
-
+};
 
 module.exports = {
   getCollections,
   postAllCollectionsToDB,
-  createAllInitialCollections
+  createAllInitialCollections,
 };
