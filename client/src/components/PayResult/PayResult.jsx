c71b9c54047d65efbdad22d1b8c42c507937ec48
Seba <sebastian.pitra10@gmail.com>
2022-12-27 16:59:37 -0300

    feat(components/PayResul): add styles and animation

--- client/src/components/PayResult/PayResult.jsx
+++ client/src/components/PayResult/PayResult.jsx
@@ -3,7 +3,9 @@ import * as actions from "../../redux/actions";
 import { useState } from "react";
 import successIcon from "../../images/icons/success-icon.png";
 import issueIcon from "../../images/icons/issue-icon.png";
+import pendingIcon from "../../images/icons/pending-icon.png";
 import styles from "./PayResult.module.css";
+import { Link } from "react-router-dom";
 
 function PayResult(props) {
   const dispatch = useDispatch();
@@ -19,52 +21,85 @@ function PayResult(props) {
 
   const sucessContainer = (
     <div className={styles["pay-result-container"]}>
+      <div className={styles["pay-result-success-line"]}></div>
       <img
         src={successIcon}
         alt="pay-icon"
         className={styles["pay-result-img-icon"]}
       />
-      <h1 className={styles["pay-result-title"]}>Success!</h1>
-      <p>We are delighted to inform you that we are received your payment</p>
-      <div>
-        <button>Back to MarketPlace</button>
-        <button>View Details</button>
+      <h1 className={styles["pay-result-success-title"]}>Success!</h1>
+      <p className={styles["pay-result-success-space-line"]}>
+        _________________
+      </p>
+      <p className={styles["pay-result-span"]}>
+        We are delighted to inform you that we are received your payment
+      </p>
+      <div className={styles["pay-result-buttons-container"]}>
+        <Link
+          className={styles["pay-result-success-marketplace-button"]}
+          to={"/marketplace"}
+        >
+          MarketPlace
+        </Link>
+        <Link className={styles["pay-result-success-details-button"]}>
+          View Details
+        </Link>
       </div>
     </div>
   );
 
   const failureContainer = (
-    <div className={styles["pay-result-container"]}>
+    <div className={styles["pay-result-failure-container"]}>
+      <div className={styles["pay-result-failure-line"]}></div>
       <img
         src={issueIcon}
         alt="pay-icon"
         className={styles["pay-result-img-icon"]}
       />
-      <h1 className={styles["pay-result-title"]}>Error!</h1>
-      <span className={styles["pay-result-span"]}>
+      <h1 className={styles["pay-result-failure-title"]}>Error!</h1>
+      <p className={styles["pay-result-failure-space-line"]}>
+        _________________
+      </p>
+      <p className={styles["pay-result-span"]}>
         Unfortunately we have an issue with your payment. Try again later
-      </span>
-      <div>
-        <button>Back to MarketPlace</button>
-        <button>View Details</button>
+      </p>
+      <div className={styles["pay-result-buttons-container"]}>
+        <Link
+          className={styles["pay-result-failure-marketplace-button"]}
+          to={"/marketplace"}
+        >
+          MarketPlace
+        </Link>
+        <Link className={styles["pay-result-failure-details-button"]}>
+          View Details
+        </Link>
       </div>
     </div>
   );
 
   const pendingContainer = (
-    <div className={styles["pay-result-container"]}>
+    <div className={styles["pay-result-pending-container"]}>
+      <div className={styles["pay-result-pending-line"]}></div>
       <img
-        src={issueIcon}
+        src={pendingIcon}
         alt="pay-icon"
         className={styles["pay-result-img-icon"]}
       />
-      <h1 className={styles["pay-result-title"]}>Pending...</h1>
-      <span className={styles["pay-result-span"]}>
-        The payment is waiting for
-      </span>
-      <div>
-        <button>Back to MarketPlace</button>
-        <button>View Details</button>
+      <h1 className={styles["pay-result-pending-title"]}>Pending...</h1>
+      <p className={styles["pay-result-pending-space-line"]}>
+        _________________
+      </p>
+      <p className={styles["pay-result-span"]}>The payment is pending</p>
+      <div className={styles["pay-result-buttons-container"]}>
+        <Link
+          className={styles["pay-result-peding-marketplace-button"]}
+          to={"/marketplace"}
+        >
+          MarketPlace
+        </Link>
+        <Link className={styles["pay-result-peding-details-button"]}>
+          View Details
+        </Link>
       </div>
     </div>
   );
