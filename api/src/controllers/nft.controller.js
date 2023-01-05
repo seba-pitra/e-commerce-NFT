200cf1c878d73f16fbdf7759019b2a9b4b9051af
franchiDev <franchi13579@gmail.com>
2023-01-05 19:06:02 -0300

    fix

--- api/src/controllers/nft.controller.js
+++ api/src/controllers/nft.controller.js
@@ -120,41 +120,6 @@ const addStarsNft = async (req, res) => {
   }
 };
 
-const addViewsNft = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const foundNft = await Nft.findByPk(id);
-    if (foundNft) {
-      foundNft.set({favs: foundNft.favs + 1});
-      await foundNft.save();
-      return res.status(200).send(foundNft);
-    } else {
-      throw new Error(`No nft with id ${id}`);
-    }
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-};
-
-const addStarsNft = async (req, res) => {
-  try {
-    const { rating } = req.body;
-    const { id } = req.params;
-    const foundNft = await Nft.findByPk(id);
-    if (foundNft) { 
-      if(rating > 0 && rating < 6) {
-        foundNft.set({stars: [...foundNft.stars, rating]});
-        await foundNft.save();
-      }
-      return res.status(200).send(foundNft);
-    } else {
-      throw new Error(`No nft with id ${id}`);
-    }
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-};
-
 //Crea el nuevo nft a partir de nombre, descripcion, imagen, contrato, id del token, precio, dueño e imagen.
 const createNewNFT = async (req, res) => {
   try {
