265f284ce4fba4b96935f7ffa595a2dc395143ff
Seba <sebastian.pitra10@gmail.com>
2023-01-07 15:09:45 -0300

    fix: firebase connection

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,9 +8,7 @@ import {
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export const firebaseConfig = JSON.parse(
-  process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_CONFIG
-);
+export const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
