82fc64d427a585c3618f959d36164378ef9e0ee5
molinaro-77 <dmolinaro96@gmail.com>
2023-01-07 20:43:36 -0300

    added loggin checker and profilepicture

---
+++ client/src/components/UserComponents/ProfilePicture/Profile.Picture.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { Avatar } from "@material-ui/core"
+
+export default function ProfilePicture({handleShowUserList}) {
+
+    const profile_picture_url = useSelector(state => state.loggedUser.profile_pic)
+    const [profilePicture, setProfilePicture] = useState(null);
+    useEffect(() => {
+        const img = new window.Image();
+        img.src = profile_picture_url;
+        console.log(img);
+        img.onload = () => setProfilePicture(img.src);
+    }, [profile_picture_url]);
+    return (
+        <div className="nav-bar-accountIcon">
+                <Avatar
+                src={profilePicture}
+                onClick={(e) => handleShowUserList(e)}
+                imgProps={{ referrerPolicy: 'no-referrer' }}
+            />
+        </div>
+    )
+}
