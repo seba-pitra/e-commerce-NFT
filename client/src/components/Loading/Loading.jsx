1cd827ddb465f9fc691376583b395c79b7165cd0
franchiDev <franchi13579@gmail.com>
2023-01-09 19:57:43 -0300

    Feat: Loading - pre Footer

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,6 +1,11 @@
-import styles from "./stylesheets/Loading.module.css";
+//dark-light theme
+import useStyles from "../../customHooks/useStyles";
+import darkStyles from "./stylesheets/DarkLoading.module.css"
+import lightStyles from "./stylesheets/LightLoading.module.css"
+
 
 export default function Loading() {
+  const styles = useStyles(darkStyles, lightStyles);
   return (
     <div className={styles["loading-page-container"]}>
       <div className={styles["loading-icon"]}>
@@ -16,6 +21,7 @@ export default function Loading() {
             <div className={styles["tri"]}></div>
             <div className={styles["tri invert"]}></div>
           </div>
+          <span>L O A D I N G</span>
         </div>
       </div>
     </div>
