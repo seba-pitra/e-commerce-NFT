d04764082a7ab5d98f23f52d09ef39e03adfad94
brunoosuna8 <brunoosuna8@gmail.com>
2023-01-10 13:18:16 -0300

    feat : real time purchases history in UserProfile,minor changes css

--- client/src/components/PurchaseHistory/PurchaseHistory.jsx
+++ client/src/components/PurchaseHistory/PurchaseHistory.jsx
@@ -1,33 +1,52 @@
 // import * as actions from "../actions/actions.js";
 import styles from "./stylesheets/PurchaseHistory.module.css";
 
-const PurchaseHistory = ({ props }) => {
-  //price,statusPay,purchases
+const PurchaseHistory = ( {purchases,sales} ) => {
+  console.log(purchases)
+  let convert = (data) =>{
+    let dateString = data;
+    let date = new Date(dateString);
+    let formattedDate = date.toLocaleString().replace(',','');
+    console.log(formattedDate);
+    return formattedDate;
+  }
+  
   return (
-    <div className={styles["detail-shopping-history"]}>
-      {props ? (
-        <div className={styles["history-title-container"]}>
-          <div className={styles["sale"]}>Sale Price</div>
-          <div className={styles["from"]}>From</div>
-          <div className={styles["to"]}>To</div>
-          <div className={styles["date"]}>Date</div>
-        </div>
-      ) : (
-        <div>There isn't shopping history</div>
-      )}
-
-      {props &&
-        props.map((elem, index) => {
-          return (
-            <div 
-            key={index}
-            className={styles["history-title-container"]}>
-              <div className={styles["from"]}>{elem.contract}</div>
-              <div className={styles["to"]}>{elem.contract}</div>
-              <div className={styles["date"]}>{elem.createdAt}</div>
-            </div>
-          );
-        })}
+    <div class="main-wrapper">
+      <div class="responsive-table">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Transaction Type</th>
+              <th scope="col">Price</th>
+              <th scope="col">Purchase Date</th>
+              <th scope="col">Payment Method</th>
+            </tr>
+          </thead>
+          <tbody>
+            {purchases && purchases.map(elem => {
+              return(<tr>
+                <td>Purchase</td>
+                <td>{elem.price}</td>
+                <td>
+                  {convert(elem.updatedAt)}
+                </td>
+                <td>{elem.payMethod}</td>
+              </tr>)
+            })}
+            {sales && sales.map(elem => {
+              return(<tr>
+                <td>Sale</td>
+                <td>{elem.price}</td>
+                <td>
+                  {convert(elem.updatedAt)}
+                </td>
+                <td>{elem.payMethod}</td>
+              </tr>)
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
