17b9762eac1148e54c9a213d9a62d265a075e333
Vaxtronn <miguel.villam96@gmail.com>
2023-01-10 17:17:45 -0500

    Fix: Links footer. Searchbar collection search

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,16 +3,29 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import SearchIcon from "@mui/icons-material/Search";
 import "./SearchBar.css";
+import { useLocation } from "react-router-dom";
 
 function SearchBar() {
+
+  const location = useLocation();
   const [searchQuery, setSearchQuery] = useState("");
   const dispatch = useDispatch();
 
+  
+
   //funcion de busqueda
   function search(e) {
-    e.preventDefault();
-    dispatch(actions.filterName(searchQuery)); //resetea los filtos
-    setSearchQuery("");
+    if(location.pathname === "/collections") {
+      e.preventDefault();
+      dispatch(actions.filterCollectionName(searchQuery));
+      setSearchQuery("");
+    }
+    else{
+      e.preventDefault();
+       //resetea los filtos
+       dispatch(actions.filterName(searchQuery)); //resetea los filtos
+      setSearchQuery("");
+    }
   }
 
   return (
