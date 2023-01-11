8bf37d8dbf156caed00eb9ab92c70bdf261873b0
valcoellar <valcoellar@gmail.com>
2023-01-11 08:11:22 -0600

    MyCollections

--- client/src/components/Mycollections/Mycollections.jsx
+++ client/src/components/Mycollections/Mycollections.jsx
@@ -5,59 +5,70 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router-dom";
-import "./Mycollections.module.css";
+//import "./Mycollections.module.css";
 import VerifiedIcon from "@mui/icons-material/Verified";
+
+import useStyles from "../../customHooks/useStyles";
+import darkStyles from "./stylesheets/DarkCollections.module.css"
+import lightStyles from "./stylesheets/LightCollections.module.css"
+
+
+
+
 
 function Mycollections() {
   const loggedUser = useSelector((state) => state.loggedUser);
   const isLoading = useSelector((state) => state.isLoading);
+  const styles = useStyles(darkStyles, lightStyles);
 
-  // -------------------
-  //console.log('HERE!!');
-  //console.log(loggedUser.collections);
-  // -------------------
+ // -------------------
+ // console.log(loggedUser.collections);
+ // -------------------
+
+
 
   const collectionsCards =
     loggedUser.collections &&
     loggedUser.collections.map((collection) => {
-      if (collection.nfts) {
-        let floorPrice = 100;
-        collection.nfts.map((nft) => {
-          if (nft.price < floorPrice) floorPrice = nft.price;
-        });
+      
+	    return (
 
-        return (
-          <Link to={`/collections/${collection.id}`} className="nolink">
-            <div className="collections-conteiner">
-              <img
-                className="collections-img-main"
-                src={collection.image}
-                alt="img-collections"
-              />
-              <div className="img-name-conteiner">
-                <img
-                  className="collections-img-owner"
-                  src={collection.nfts[0].image}
-                  alt="img-collections"
-                />
-                <div>
-                  <div className="collection-name-conteiner">
-                    <VerifiedIcon />
-                    <h3 className="collections-name"> {collection.name} </h3>
-                  </div>
-                  <div className="collection-name-conteiner">
-                    <h3 className="collections-name">
-                      {" "}
-                      Floor Price ETH: {floorPrice.toFixed(3)}
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        );
-      }
-    });
+ <Link to={`/collections/${collection.id}`} className={styles["link"]}>
+
+		    <div className={styles["collections-container"]}>
+		    <img
+		    className={styles["collections-img-main"]}
+		    src="https://res.cloudinary.com/dwyhztlkw/image/upload/v1673373417/xx83ajscnftifvyajlrj.jpg"
+		    alt="img-collections"
+		    />
+		    <div className={styles["img-name-container"]}>
+		    <img
+		    className={styles["collections-img-owner"]}
+		    src="https://res.cloudinary.com/dwyhztlkw/image/upload/v1673373417/xx83ajscnftifvyajlrj.jpg"
+		    alt="img-collections"
+		    />
+		    <div>
+		    <div className={styles["collection-name-container"]}>
+		    <VerifiedIcon />
+		    <h3 className={styles["collections-name"]}>
+		    {collection.name}
+		    </h3>
+		    </div>
+		    <div className={styles["collection-name-container"]}>
+		    <h3 className={styles["collections-name"]}>
+		    Contract: {collection.contract}
+		    </h3>
+		    </div>
+		    </div>
+		    </div>
+		    </div>
+
+</Link>
+
+
+	    );
+    }
+    );
 
   return (
     <div className="conteiner-main-collections">
