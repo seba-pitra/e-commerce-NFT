4f691bb5c135f171428ff9b913ec9e985484c79e
molinaro-77 <dmolinaro96@gmail.com>
2023-01-10 23:46:05 -0300

    fixed bad sequelize url call

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, DB_DEPLOY2 } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/test`,
+  DB_DEPLOY,
   {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
