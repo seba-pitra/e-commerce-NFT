12c081f14eaf14b357a4cb6c53d5d52c1c7fe54d
brunoosuna8 <brunoosuna8@gmail.com>
2023-01-11 22:56:59 -0300

    fix: PurchaseHistory comp

--- client/src/components/PurchaseHistory/PurchaseHistory.jsx
+++ client/src/components/PurchaseHistory/PurchaseHistory.jsx
@@ -1,60 +1,49 @@
-import darkStyles from "./stylesheets/DarkPurchaseHistory.module.css";
-import lightStyles from "./stylesheets/LightPurchaseHistory.module.css";
-import useStyles from "../../customHooks/useStyles";
+// import * as actions from "../actions/actions.js";
+import styles from "./stylesheets/PurchaseHistory.module.css";
 
-const PurchaseHistory = ({ purchases, sales }) => {
-  const styles = useStyles(darkStyles, lightStyles);
-
-  let convert = (data) => {
+const PurchaseHistory = ( {purchases,sales} ) => {
+  console.log(purchases)
+  let convert = (data) =>{
     let dateString = data;
     let date = new Date(dateString);
-    let formattedDate = date.toLocaleString().replace(",", "");
+    let formattedDate = date.toLocaleString().replace(',','');
+    console.log(formattedDate);
     return formattedDate;
-  };
-
+  }
+  
   return (
-    <div className={styles["main-wrapper"]}>
-      <div className={styles["responsive-table"]}>
-        <table className={styles["table"]}>
+    <div class="main-wrapper">
+      <div class="responsive-table">
+        <table class="table">
           <thead>
-            <tr className={styles["tr"]}>
-              <th className={styles["col"]} scope="col">
-                Transaction Type
-              </th>
-              <th className={styles["col"]} scope="col">
-                Price
-              </th>
-              <th className={styles["col"]} scope="col">
-                Purchase Date
-              </th>
-              <th className={styles["col"]} scope="col">
-                Payment Method
-              </th>
+            <tr>
+              <th scope="col">Transaction Type</th>
+              <th scope="col">Price</th>
+              <th scope="col">Purchase Date</th>
+              <th scope="col">Payment Method</th>
             </tr>
           </thead>
           <tbody>
-            {purchases &&
-              purchases.map((elem) => {
-                return (
-                  <tr>
-                    <td>Purchase</td>
-                    <td>{elem.price} ETH</td>
-                    <td>{convert(elem.updatedAt)}</td>
-                    <td>{elem.payMethod}</td>
-                  </tr>
-                );
-              })}
-            {sales &&
-              sales.map((elem) => {
-                return (
-                  <tr>
-                    <td>Sale</td>
-                    <td>{elem.price}</td>
-                    <td>{convert(elem.updatedAt)}</td>
-                    <td>{elem.payMethod}</td>
-                  </tr>
-                );
-              })}
+            {purchases && purchases.map(elem => {
+              return(<tr>
+                <td>Purchase</td>
+                <td>{elem.price}</td>
+                <td>
+                  {convert(elem.updatedAt)}
+                </td>
+                <td>{elem.payMethod}</td>
+              </tr>)
+            })}
+            {sales && sales.map(elem => {
+              return(<tr>
+                <td>Sale</td>
+                <td>{elem.price}</td>
+                <td>
+                  {convert(elem.updatedAt)}
+                </td>
+                <td>{elem.payMethod}</td>
+              </tr>)
+            })}
           </tbody>
         </table>
       </div>
